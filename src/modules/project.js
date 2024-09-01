0d77e2872e1eeeb13f44c48996e0aad6148ea239
dekr1sh <krishsonigra2175@gmail.com>
2024-09-01 12:12:57 +0530

    Update the logic for handling projects

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,7 +1,28 @@
-export const createProject = (name) => {
-    return { name, todos: [] };
+const projects = []; 
+
+const getProjects = () => {
+    return projects;
 };
 
-export const addTodoToProject = (project, todo) => {
-    project.todos.push(todo);
+const createProject = (name) => {
+    const project = { name, tasks: [] };
+    projects.push(project);
+    return project;
 };
+
+const renameProject = (project, newName) => {
+    project.name = newName;
+};
+
+const deleteProject = (projectToDelete) => {
+    const index = projects.indexOf(projectToDelete);
+    if (index !== -1) {
+        projects.splice(index, 1);
+    }
+};
+
+const addTaskToProject = (project, task) => {
+    project.tasks.push(task);
+};
+
+export { createProject, renameProject, deleteProject, addTaskToProject, getProjects };
