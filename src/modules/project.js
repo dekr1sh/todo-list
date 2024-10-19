84546021ff13f8c1fb4917ae879c13a1fccdc78b
dekr1sh <krishsonigra2175@gmail.com>
2024-10-19 23:14:25 +0530

    Remove the getCurrentDataProject function
    The said function was found to be redundant and thus was removed from
    project.js module.

--- src/modules/project.js
+++ src/modules/project.js
@@ -8,14 +8,9 @@ function createProject(dataProject, name) {
     return { dataProject, name, tasks: [] };
 };
 
-function getCurrentDataProject() {
-    const projects = getProjects();
-    return projects.length - 1;
-}
-
 function getNextDataProject() {
     const projects = getProjects();
     return projects.length;
 }
 
-export { getProjects, createProject, getCurrentDataProject, getNextDataProject };
+export { getProjects, createProject, getNextDataProject };
